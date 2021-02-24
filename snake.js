25e8ee2380e7410bb091734f3caacaf50dd45cf1
jayyp35 <jprabhu35@gmail.com>
2021-02-25 00:42:25 +0530

    Reversed cells[] array logic. Previous logic was not allowing addition of cells

--- snake.js
+++ snake.js
@@ -5,14 +5,14 @@ function init() {
     pen = canvas.getContext('2d')
     w = 500
     h = 500
-    cs =500/15
+    cs =250/15
     snake = {
         init_length: 4,
         color: "blue",
         direction: "right",
         cells:[],
         createSnake: function() {
-            for(var i=1;i<=this.init_length;i++) {
+            for(var i=this.init_length;i>=1;i--) {
                 this.cells.push({x:i,y:0})
             }
         },
@@ -20,36 +20,40 @@ function init() {
             console.log("drawing",this.cells);
             pen.fillStyle = "#f0a500";
             this.cells.forEach((data,index) => {
-                pen.fillRect(cs*(data.x),cs*(data.y),cs,cs)
+                pen.fillRect(this.cells[index].x * cs,this.cells[index].y * cs,cs+1,cs+1)
             })
             
         },
         updateSnake : function() {
-            
-            if((this.cells[this.init_length - 1].x == food.x) &&
-                (this.cells[this.init_length - 1].y == food.y)) {
-                    getRandomFood();
-                    
-                }
-            
-            var X = this.cells[this.init_length - 1].x    
-            var Y = this.cells[this.init_length - 1].y
-            this.cells.shift()
+            var X = this.cells[0].x    
+            var Y = this.cells[0].y
+
+            if((this.cells[0].x == food.x) &&
+                (this.cells[0].y == food.y)) {
+                    getRandomFood(); 
+            } else {
+                this.cells.pop()
+            }   
+            var nX,nY;
 
             if(this.direction == 'right') { 
-                X = X + 1;
+                nX = X + 1;
+                nY = Y;
             }
             else if(this.direction == 'down'){
-                Y = Y + 1; 
+                nX = X;
+                nY = Y + 1;
             }
             else if(this.direction == 'left'){
-                X = X - 1;  
+                nX = X - 1;
+                nY = Y; 
             }
             else if(this.direction == 'up'){  
-                Y = Y - 1;
+                nX = X;
+                nY = Y-1;
             }
 
-            this.cells.push({x:X,y:Y}) 
+            this.cells.unshift({x:nX,y:nY}) 
 
 
         },
@@ -97,8 +101,8 @@ function getRandomFood() {
 }
 
 function gameloop() {
-    cellx = snake.cells[snake.init_length - 1].x
-    celly = snake.cells[snake.init_length - 1].y
+    cellx = snake.cells[0].x
+    celly = snake.cells[0].y
     
     if( ((cellx)*(cs) > 500) || (((cellx+0)*cs) < 0)
         ||(((celly)*cs) > 500) || ((celly)*(cs) < 0) )
